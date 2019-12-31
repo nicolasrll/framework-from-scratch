a6f6cf2a5a6c67d567cd9cac9dadb6ccea865b21
Nicolas Rellier <nicolasrellier@yahoo.fr>
2019-12-31 14:49:00 +0100

    Create structure files and basic tags

---
+++ articles.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Liste des articles</title>
+	</head>
+
+	<body>
+		<header>			
+		</header>
+		
+		<main>			
+		</main>
+		
+		<footer>		
+		</footer>
+	</body>
+
+</html> 
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Accueil</title>
+	</head>
+
+	<body>
+		<header>			
+		</header>
+		
+		<main>
+		</main>
+
+		<footer>
+		</footer>
+	</body>
+
+</html> 

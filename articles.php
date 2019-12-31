423e87fc34e1742e5f57f4cce61f07a35af02883
Nicolas Rellier <nicolasrellier@yahoo.fr>
2019-12-31 15:36:20 +0100

    Replace header and footer by include to have less redundant code

--- articles.php
+++ articles.php
@@ -6,23 +6,14 @@
 	</head>
 
 	<body>
-		<header>
-			<nav>
-				<ul>
-					<li><a href="index.php">Accueil</a></li>
-					<li><a href="authentification.php">Inscription/Connexion</a></li>
-					<li><a href="articles.php">Articles</a></li>
-					<li><a href="article.php">Article</a></li>
-				</ul>
-			</nav>		
-		</header>
+		<?php require "_header.php"; ?>
+
 		
 		<main>
 			<h1>Articles</h1>		
 		</main>
-		
-		<footer>		
-		</footer>
+
+		<?php require "_footer.php"; ?>
 	</body>
 
 </html> 

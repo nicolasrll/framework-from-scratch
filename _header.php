b5e1559c4e2b0086eff7073773dd4420910ed4fc
Nicolas Rellier <nicolasrellier@yahoo.fr>
2019-12-31 15:25:34 +0100

    Create and include _header.php and _footer.php

---
+++ _header.php
@@ -0,0 +1,10 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="index.php?page=authentification">Inscription/Connexion</a></li>
+			<li><a href="index.php?page=articles">Articles</a></li>
+			<li><a href="index.php?page=article">Article</a></li>
+		</ul>
+	</nav>
+</header>

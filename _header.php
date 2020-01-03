eeda3e6c2aeb2fa60d9226d95c75b58deb85f546
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-01-03 11:23:09 +0100

    Transform .php files to .html

--- _header.php
+++ _header.php
@@ -1,10 +1,10 @@
 <header>
 	<nav>
 		<ul>
-			<li><a href="index.php">Accueil</a></li>
-			<li><a href="index.php?page=authentification">Inscription/Connexion</a></li>
-			<li><a href="index.php?page=articles">Articles</a></li>
-			<li><a href="index.php?page=article">Article</a></li>
+			<li><a href="index.html">Accueil</a></li>
+			<li><a href="index.html?page=authentification">Inscription/Connexion</a></li>
+			<li><a href="index.html?page=articles">Articles</a></li>
+			<li><a href="index.html?page=article">Article</a></li>
 		</ul>
 	</nav>
 </header>

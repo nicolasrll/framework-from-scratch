1ac204a3c1026a26a8a7f48decd7f3eaedb33379
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-01-03 20:46:06 +0100

    3.2 : Modification to remove index.php on hrefs

--- _header.php
+++ _header.php
@@ -1,10 +1,17 @@
 <header>
 	<nav>
 		<ul>
+			<!--
 			<li><a href="index.html">Accueil</a></li>
 			<li><a href="index.html?page=authentification">Inscription/Connexion</a></li>
 			<li><a href="index.html?page=articles">Articles</a></li>
 			<li><a href="index.html?page=article">Article</a></li>
+			-->
+			<li><a href="/?page=accueil">Accueil</a></li>
+			<li><a href="/?page=authentification">Inscription/Connexion</a></li>
+			<li><a href="/?page=articles">Articles</a></li>
+			<li><a href="/?page=article">Article</a></li>
+
 		</ul>
 	</nav>
 </header>

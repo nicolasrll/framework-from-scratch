de7be59d2ee17884538f760864287e06e178cf88
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-01-04 14:29:59 +0100

    clean index

--- index.php
+++ index.php
@@ -4,31 +4,24 @@
 		<meta charset="UTF-8">
 		<title>Accueil</title>
 	</head>
-
 	<body>
 		<?php require "_header.php"; ?>
-		
 		<main>
 			<h1>Accueil</h1>
-
 			<?php
-				switch($_GET['page']){					
+				switch($_GET['page']){
 					case "article":
-						$content = require "article.php";
+						require "article.php";
 						break;
 					case "articles":
-						$content = require "articles.php";
+						require "articles.php";
 						break;
 					case "authentification":
-						$content = require "authentification.php";
+						require "authentification.php";
 						break;
 				}
-
-				$content;
 			?>
 		</main>
-
 		<?php require "_footer.php"; ?>
 	</body>
-
-</html> 
+</html>

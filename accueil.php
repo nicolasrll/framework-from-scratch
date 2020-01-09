7b7ac9dfe8652cd29d66cf50f4b14dad29fabeba
Florent Moy <florent.moy@gmail.com>
2020-01-09 19:34:57 +0100

    feature 4

--- accueil.php
+++ accueil.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Accueil</title>
-	</head>
-
-	<body>
-		<main>
-			<h1>Accueil</h1>
-		</main>
-	</body>
-</html>
+<h1>Accueil</h1>

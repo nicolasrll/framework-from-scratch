0f5e065963842b31c7f92b3e8951776d704c07ef
Florent Moy <florent.moy@gmail.com>
2020-01-09 19:34:57 +0100

    feature 4

---
+++ Controller.php
@@ -0,0 +1,13 @@
+<?php
+    function index(){
+        require "accueil.php";
+    }
+    function article(){
+        require "article.php";
+    }
+    function articles(){
+        require"articles.php";
+    }
+    function authentification(){
+        require "authentification.php";
+    }
---
+++ accueil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Accueil</title>
+	</head>
+
+	<body>		
+		<main>
+			<h1>Accueil</h1>
+		</main>
+	</body>
+</html> 

5df9c3a3dbaa3710bdcab5d472d201f800d69b29
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-01-16 09:39:07 +0100

    feat : issue #15 done (step 6)

--- index.php
+++ index.php
@@ -12,27 +12,29 @@
 				$uri = $_SERVER['REQUEST_URI'];
 				$uri = trim(parse_url($uri, PHP_URL_PATH), "/");
 				$uri = explode("/", $uri);
-				$controller =  $uri[0];
-				$action = $uri[1];
+				if(isset($uri[0])){
+					$controller =  $uri[0];
+				}
+				require "Controllers/DefaultController.php";
 				switch($controller){
 					case "accueil":
 						require "Controllers/AccueilController.php";
-						$accueilController = new AccueilController;
+						$accueilController = new AccueilController();
 						$accueilController->index();
 						break;
 					case "article":
 						require "Controllers/ArticleController.php";
-						$articleController = new ArticleController;
+						$articleController = new ArticleController();
 						$articleController->index();
 						break;
 					case "articles":
 						require "Controllers/ArticlesController.php";
-						$articlesController = new ArticlesController;
+						$articlesController = new ArticlesController();
 						$articlesController->index();
 						break;
 					case "authentification":
 						require "Controllers/AuthentificationController.php";
-						$authentificationController = new AuthentificationController;
+						$authentificationController = new AuthentificationController();
 						$authentificationController->index();
 						break;
 				}
